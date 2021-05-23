88a04e74df801947c82c7433b9de0b32fdf3600f
Al Joseph <condinoaljoseph@gmail.com>
2021-05-23 18:28:25 +0800

    open site on notification

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const qs = require('qs');
 const cron = require('node-cron');
 const axios = require('axios');
 const notifier = require('node-notifier');
+const open = require('open');
 
 const data = qs.stringify({
 	maxDate: '2021-11-30',
 	requestDate: '2021-05-26',
-	siteId: '17',
+	siteId: '14',
 	slots: '3'
 });
 
@@ -19,7 +20,7 @@ const config = {
 	data: data
 };
 
-const task = cron.schedule('*/10 * * * *', () => {
+const task = cron.schedule('*/1 * * * *', () => {
 	console.log(
 		'Checking available dates on Cebu (Pacific Mall Metro Mandaue, Cebu) DFA Regional Consular Office – Cebu'
 	);
@@ -27,12 +28,15 @@ const task = cron.schedule('*/10 * * * *', () => {
 	axios(config)
 		.then(function (response) {
 			if (response.data.hasOwnProperty('Date')) {
-				notifier.notify({
-					title: 'Passport Appointment Available',
-					message: `Earliest Date Available: ${new Date(
-						response.data.Date
-					).toDateString()}`
-				});
+				notifier.notify(
+					{
+						title: 'Passport Appointment Available',
+						message: `Earliest Date Available: ${new Date(
+							response.data.Date
+						).toDateString()}`
+					},
+					() => open('https://www.passport.gov.ph/appointment')
+				);
 				task.stop();
 			}
 		})
